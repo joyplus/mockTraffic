b54808b36a1e85fffb92b7a4d8b2fabf21165fe2
cloverstd <cloverstd@gmail.com>
2014-08-22 16:12:13 +0800

    添加 web supervisor 配置文件，优化 fabric 配置

--- supplement/fabfile.py
+++ supplement/fabfile.py
@@ -3,16 +3,24 @@
 
 from fabric.api import local, settings, env, put, cd, run, hide
 from fabric.colors import green, blue, red
+import os.path
 
 env.hosts = ["10.6.2.12"]
 env.user = "yangyi"
+package_name="supplement.tar.gz" # 打包的文件名
+code_dir = "/home/yangyi/supplement" # 服务器代码目录
+config_file_path = "/home/yangyi" # 服务器代码配置文件的目录
+host_python_path = "/home/yangyi/.virtualenv/supplement" # 服务器 python 环境
 
-package_name="supplement.tar.gz"
-host_python_path = "/home/yangyi/.virtualenv/supplement"
-host_python_env = "/home/yangyi/.virtualenv/supplement/bin/python"
-host_python_bin = "/home/yangyi/.virtualenv/supplement/bin"
 
-code_dir = "/home/yangyi/supplement"
+# supplement cli 配置文件
+supplement_config_file = os.path.join(config_file_path, "supplement.ini")
+# supplement web 配置文件
+web_config_file = os.path.join(config_file_path, "config.py")
+
+host_python_env = os.path.join(host_python_path, "bin/python")
+host_python_bin = os.path.join(host_python_path, "bin")
+
 
 def package():
     exclude_file = ["*.log", "supplement.tar.gz", "*.pyc", "*.supplement"]
@@ -41,8 +49,10 @@ def deploy():
     with settings(hide("stdout")):
         run("{python_bin}/pip install -r {code_dir}/requirements.txt".format(python_bin=host_python_bin,
                                                                              code_dir=code_dir))
-    run("cp /home/yangyi/supplement.ini {code_dir}/config.ini".format(code_dir=code_dir))
-    run("cp /home/yangyi/config.py {code_dir}/web".format(code_dir=code_dir))
+    run("cp {supplement_config_file} {code_dir}/config.ini".format(supplement_config_file=supplement_config_file,
+                                                                   code_dir=code_dir))
+    run("cp {web_config_file} {code_dir}/web".format(web_config_file=web_config_file,
+                                                     code_dir=code_dir))
 
 def clear():
     rm_package = "rm -rf {package_name}".format(package_name=package_name)
